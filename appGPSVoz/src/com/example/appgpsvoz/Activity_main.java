692bfbb99261a405c4c178f7e3ce2c729842bc18
Luis Alejandro <l.alejandro.gb@gmail.com>
2015-02-04 18:13:24 +0100

    Created appGPSVoz icon

--- appGPSVoz/src/com/example/appgpsvoz/Activity_main.java
+++ appGPSVoz/src/com/example/appgpsvoz/Activity_main.java
@@ -1,22 +1,34 @@
+/*
+ *  Copyright (C) 2014, 2015 - Luis Alejandro González Borrás, Jose Manuel Gómez González>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.example.appgpsvoz;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import android.speech.RecognizerIntent;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +36,13 @@ import android.widget.Button;
 import android.widget.Toast;
 import com.example.appgpsvoz.InvalidSpeechException;
 
+/** Clase para la actividad principal
+ * 
+ * @author Luis Alejandro Gonzalez Borras
+ * @author Jose Manuel Gomez Gonzalez
+ * @version 1.0
+ * @see Visitar www.github.com/LuisGonzalez2014/Android_Projects/tree/master/appGPSVoz
+ */
 public class Activity_main extends Activity {
 	private final static String DEFAULT_LANG_MODEL = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM; 
 	private String languageModel; 
@@ -97,8 +116,14 @@ public class Activity_main extends Activity {
         if (requestCode == ASR_CODE)  {
             if (resultCode == RESULT_OK)  {            	
             	if(data!=null) {
+            		// Obtenemos las cadenas reconocidas
             		ArrayList<String> coincidenceList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-					final String cadena = coincidenceList.get(0);
+					// Almacenamos la primera cadena de las reconocidas
+            		final String cadena = coincidenceList.get(0);
+            		
+            		//final String cadena = "latitud 37 grados 11 minutos 48 con 4 segundos norte longitud 3 grados 37 minutos 28 con 8 segundos este";
+            		
+            		// Obtenemos la localización del dispositivo por medio de la Internet
 					location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 					
 					if (location != null)
@@ -108,25 +133,29 @@ public class Activity_main extends Activity {
 						
 						double latitud_destino = 0, longitud_destino = 0;
 
+						// Expresiones regulares para la extracción de los datos
 				        Pattern top = Pattern.compile("latitud (.+) longitud (.+)");
 				        Pattern def1 = Pattern.compile("^((?:menos )?\\d+)(?: con | coma | punto )?(\\d+)?$");
 				        Pattern def2 = Pattern.compile("^((?:menos )?\\d+) grados? ((?:menos )?\\d+) minutos? ((?:menos )?\\d+)(?: con | coma | punto )?(\\d+)? segundos? (norte|sur|este|oeste)$");
 				        Matcher mTop = top.matcher(cadena.toLowerCase());
-
+				        
+				        /*
+	            		Toast toast2 = Toast.makeText(getApplicationContext(),"Cadena: " + cadena, Toast.LENGTH_LONG);
+						toast2.show();
+						*/
 				        try{
 					        if (mTop.find()) {
 					            Matcher mDef1 = def1.matcher(mTop.group(1));
 					            Matcher mDef2 = def2.matcher(mTop.group(1));
 	
 					            latitud_destino = getCoordinateValue(mDef1, mDef2);
-	
 					            mDef1 = def1.matcher(mTop.group(2));
 					            mDef2 = def2.matcher(mTop.group(2));
 	
 					            longitud_destino = getCoordinateValue(mDef1, mDef2);
 					            System.out.println("Latitud: "+latitud_destino+" Longitud: "+longitud_destino);
 					        } else {
-					            throw new InvalidSpeechException("No se ha reconocido su voz");
+					            throw new InvalidSpeechException("ERROR: No se ha detectado correctamente su voz");
 					        }
 				        }catch(InvalidSpeechException e){}
 
@@ -150,6 +179,13 @@ public class Activity_main extends Activity {
         }
 	}
 	
+	/**
+	 * 
+	 * @param m1 coordenadas latitud
+	 * @param m2 coordenadas longitud
+	 * @return la conversión de grados, minutos y segundos a decimal
+	 * @throws InvalidSpeechException
+	 */
 	private static double getCoordinateValue(Matcher m1, Matcher m2) throws InvalidSpeechException {
         double valor = 0;
 
